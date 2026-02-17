d4c99439cced32e8f65b7111e1e6d01fb3b7ac5b
MacG <kemutan0909@gmail.com>
2026-02-17 10:37:25 +0900

    敵がダメージを受けたら死ぬように

--- Assets/Scripts/StageSystem/Enemy/Enemy.cs
+++ Assets/Scripts/StageSystem/Enemy/Enemy.cs
@@ -7,11 +7,19 @@ public class Enemy : MonoBehaviour,IEnemy
 {
     [SerializeField] float damage = 1f;
     
+    /// <summary>
+    /// ダメージを受けた時の処理
+    /// </summary>
+    /// <param name="damage">ダメージ量</param>
     public void TakeDamage(float damage)
     {
         Destroy(gameObject);
     }
 
+    /// <summary>
+    /// プレイヤーに触れた時の処理
+    /// </summary>
+    /// <param name="collision"></param>
     void OnCollisionEnter(Collision collision)
     {
         var player = collision.gameObject.GetComponent<PlayerHPManager>();
